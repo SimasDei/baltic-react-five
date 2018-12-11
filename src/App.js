1c906eb58c7abb4e4e25e57ae8092590a80d0b83
SimasDei <simondeiman@gmail.com>
2018-12-11 13:49:57 +0200

    Adding Routes to a blog system

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import React, {Component} from 'react';
-import './App.css';
-import Header from './components/Header';
-import Home from './components/Home';
+import React, { Component } from 'react';
+
+import Blog from './containers/Blog/Blog';
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="row bg-dark">
-          <Header/>
-        </div>
-        <div className="row">
-          <div className="col-xs-10 col-xs-offset-1">
-            <Home/>
-          </div>
-        </div>
+      <div className="App">
+        <Blog />
       </div>
     );
   }
